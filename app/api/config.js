7c0b78c8480f382b804daecdf94b736a4c939ff6
unknown <leonidtatarchenkov@gmail.com>
2024-05-28 22:27:52 +0500

    1

--- app/api/config.js
+++ app/api/config.js
@@ -1,6 +1,6 @@
 // Мы используем домен бэкенда, который создали для этого урока,
 // а тебе нужно будет указать свой
-export const BASE_URL = 'https://pindie.back.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://backend-rabota.nomorepartiesco.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
